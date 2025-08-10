706ce0c442f462ca1c7baa4d253c81eec34925d1
Edielson-Rodrigues <edielsonrodrigues567@gmail.com>
2025-08-10 00:32:37 -0300

    refactor: update sections

--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,5 +1,7 @@
 import { AboutSection } from './about';
 import { ExperienceSection } from './experience';
 import { HeroSection } from './hero';
+import { PortfolioSection } from './portfolio';
+import { SkillsSection } from './skills';
 
-export { AboutSection, ExperienceSection, HeroSection };
+export { AboutSection, ExperienceSection, HeroSection, PortfolioSection, SkillsSection };
